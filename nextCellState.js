0fba5bdb72b85fa64c45c3a1ea3e4c912cec24f2
joe-butler-dev <joefish.b@gmail.com>
2020-06-02 20:12:42 +1200

    Complete nextCellState function

--- nextCellState.js
+++ nextCellState.js
@@ -3,7 +3,19 @@ const isUnderPopulated = require('./isUnderPopulated')
 const isRessurectable = require('./isRessurectable')
 
 function nextCellState (cellState, neighbourCount) {
-
+    if (cellState == 1) {
+        if (isOverPopulated(neighbourCount) || isUnderPopulated(neighbourCount)) {
+            return 0
+        } else {
+            return 1
+        }
+    } else if (cellState == 0) {
+        if (isRessurectable(neighbourCount)) {
+            return 1
+        } else {
+            return 0
+        }
+    }
 }
 
 module.exports = nextCellState
